286b19b46a23a9ca19c85b4c9f4632c88bb79143
fenix-cr <icol2006@hotmail.com>
2023-02-13 23:20:05 -0600

    change api

--- src/libs/axios.js
+++ src/libs/axios.js
@@ -5,8 +5,8 @@ import axios from 'axios'
 
 const axiosIns = axios.create({
  //baseURL: 'https://ameraapi.vnddev.com/public/api/v1/'
-//baseURL: 'http://127.0.0.1:8000/api/v1/'
-   baseURL: 'https://api.myamera.com/api/v1/'
+baseURL: 'http://127.0.0.1:8000/api/v1/'
+ //  baseURL: 'https://api.myamera.com/api/v1/'
 })
 
 axiosIns.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
